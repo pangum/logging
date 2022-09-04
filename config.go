286d7da7d4b69478f512aaa2f8b68600ff9bc85c
storezhang <storezhang@gmail.com>
2022-09-04 18:05:13 +0800

    bugfix(日志): 修复日志记录层级

--- config.go
+++ config.go
@@ -6,7 +6,7 @@ type config struct {
 	// 日志类型
 	Type string `default:"zap" json:"type" yaml:"type" toml:"type" validate:"required,oneof=zap system logrus zero"`
 	// 日志调用方法过滤层级
-	Skip int `default:"3" json:"skip" yaml:"skip" toml:"skip"`
+	Skip int `default:"2" json:"skip" yaml:"skip" toml:"skip"`
 	// 调用堆栈层级
 	Stacktrace *stacktrace `json:"stacktrace" yaml:"stacktrace" toml:"stacktrace"`
 }
